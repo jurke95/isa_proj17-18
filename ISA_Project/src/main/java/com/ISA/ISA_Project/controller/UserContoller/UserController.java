e98070985605eb683399b3f3801a8edc866c5247
DAMJAN\Daki PRO <nicicdamjan@gmail.com>
2018-03-13 18:36:36 +0100

    edit User

--- ISA_Project/src/main/java/com/ISA/ISA_Project/controller/UserContoller/UserController.java
+++ ISA_Project/src/main/java/com/ISA/ISA_Project/controller/UserContoller/UserController.java
@@ -12,48 +12,80 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
+	// @RequestMapping(value ="/registration",method= RequestMethod.POST)
+	@PostMapping("/registration")
+	public MessageResponseDTO registration(@RequestBody RegistrationDTO registrationDTO) {
 
-//    @RequestMapping(value ="/registration",method= RequestMethod.POST)
-    @PostMapping("/registration")
-    public MessageResponseDTO registration(@RequestBody RegistrationDTO registrationDTO){
+		User user = new User();
 
-        User user = new User();
- 
-        user.setEmail(registrationDTO.getEmail());
-        if(registrationDTO.getPassword1().equals((registrationDTO.getPassword2())))
-        	user.setPassword(registrationDTO.getPassword1());
-        else return new MessageResponseDTO("password1 and password2 are not equal");
-        user.setName(registrationDTO.getName());
-        user.setSurname(registrationDTO.getSurname());
-        user.setCity(registrationDTO.getCity());
-        user.setPhoneNumber(registrationDTO.getPhoneNumber());
-        if(!(userService.checkUniqueEmail(registrationDTO.getEmail()))) {
-        	return new MessageResponseDTO("This email already exists");
-        }
-        User temp = userService.registrateUser(user);
+		if (!(userService.checkUniqueEmail(registrationDTO.getEmail()))) {
+			return new MessageResponseDTO("This email already exists");
+		}
+		user.setEmail(registrationDTO.getEmail());
+		if (registrationDTO.getPassword1().equals((registrationDTO.getPassword2())))
+			user.setPassword(registrationDTO.getPassword1());
+		else
+			return new MessageResponseDTO("password1 and password2 are not equal");
+		user.setName(registrationDTO.getName());
+		user.setSurname(registrationDTO.getSurname());
+		user.setCity(registrationDTO.getCity());
+		user.setPhoneNumber(registrationDTO.getPhoneNumber());
 
-        if(temp == null){
-            return new MessageResponseDTO("User is not registrated");
-        }
+		User temp = userService.registrateUser(user);
 
-        return new MessageResponseDTO("User is registrated");
-    }
-    
-    @PostMapping("/login")
-    public MessageResponseDTO login(@RequestBody LoginDTO loginDTO){
-    	User temp = userService.findOneUserByEmail(loginDTO.getEmail());
-    	
-    	if(temp == null)
-    		return new MessageResponseDTO("Invalid email address");
-    	if(!(temp.getPassword().equals(loginDTO.getPassword())))
-    		return new MessageResponseDTO("Invalid password");
-    	if(!(temp.isActive()))
-    		return new MessageResponseDTO("User is not active");
-    	
-    	
-    	return new MessageResponseDTO("Login success");
-    }
+		if (temp == null) {
+			return new MessageResponseDTO("User is not registrated");
+		}
+
+		return new MessageResponseDTO("User is registrated");
+	}
+
+	@PostMapping("/login")
+	public MessageResponseDTO login(@RequestBody LoginDTO loginDTO) {
+		User temp = userService.findOneUserByEmail(loginDTO.getEmail());
+
+		if (temp == null)
+			return new MessageResponseDTO("Invalid email address");
+		if (!(temp.getPassword().equals(loginDTO.getPassword())))
+			return new MessageResponseDTO("Invalid password");
+		if (!(temp.isActive()))
+			return new MessageResponseDTO("User is not active");
+
+		return new MessageResponseDTO("Login success");
+	}
+
+	@PostMapping("/edituser")
+	public MessageResponseDTO edituser(@RequestBody RegistrationDTO registrationDTO) {
+
+		User user = new User();
+		// user= getsesion....
+		// if(!(registrationDTO.getEmail().equals(getSession.getEmail()))){
+		if (!(userService.checkUniqueEmail(registrationDTO.getEmail()))) {
+			return new MessageResponseDTO("This email already exists");
+		} else {
+			user.setEmail(registrationDTO.getEmail()); // Must make a activate link for email
+		}
+
+		if (registrationDTO.getPassword1().equals((registrationDTO.getPassword2())))
+			user.setPassword(registrationDTO.getPassword1());
+		else
+			return new MessageResponseDTO("password1 and password2 are not equal");
+		user.setName(registrationDTO.getName());
+		user.setSurname(registrationDTO.getSurname());
+		user.setCity(registrationDTO.getCity());
+		user.setPhoneNumber(registrationDTO.getPhoneNumber());
+
+		// if Activation on the email is accepted
+
+		User temp = userService.registrateUser(user);
+
+		if (temp == null) {
+			return new MessageResponseDTO("User is not registrated");
+		}
+
+		return new MessageResponseDTO("User is registrated");
+	}
 }
